4866beac78f085210c18da2ba3ece22580ef6357
jungdonguk <foodsmith96@naver.com>
2023-04-23 18:46:07 +0900

    final

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -5,9 +5,18 @@ import lombok.Data;
 @Data
 public class Item {
 
+    //    @NotNull(groups = UpdateCheck.class)
     private Long id;
+
+    //    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
     private String itemName;
+
+    //    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+//    @Range(min = 1000, max = 1000000, groups = {SaveCheck.class, UpdateCheck.class})
     private Integer price;
+
+    //    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+//    @Max(9999)
     private Integer quantity;
 
     public Item() {
